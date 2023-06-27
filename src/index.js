510af9111565a7f43121d2938dc92bfc686652c3
Arnav Savant <savantishu02@gmail.com>
2023-06-28 02:31:35 +0530

    Created user model

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const {serverConfig, Logger} = require('./config');
 const routes = require('./routes');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.use('/api',routes);
 
