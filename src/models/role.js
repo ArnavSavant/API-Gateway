dca6dfa95e87667ee1f37d839965d16d940d1870
Arnav Savant <savantishu02@gmail.com>
2023-07-03 15:21:27 +0530

    Added Roles model and set us many to many assosciations between Roles and User models through a join table

---
+++ src/models/role.js
@@ -0,0 +1,31 @@
+"use strict";
+const { Model } = require("sequelize");
+const Enum = require("../utils/common/enum");
+const { ADMIN, CUSTOMER, FLIGHT_COMPANY } = Enum.USER_ROLES;
+module.exports = (sequelize, DataTypes) => {
+	class Role extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			this.belongsToMany(models.User, { through: "User_Roles", as: "Role" });
+		}
+	}
+	Role.init(
+		{
+			name: {
+				type: DataTypes.ENUM,
+				values: [ADMIN, CUSTOMER, FLIGHT_COMPANY],
+				defaultValue: CUSTOMER,
+				allowNull: false,
+			},
+		},
+		{
+			sequelize,
+			modelName: "Role",
+		}
+	);
+	return Role;
+};
